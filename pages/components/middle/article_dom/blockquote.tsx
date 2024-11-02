1db86f143a12a99f1a0fadb3bff03c9ff3ce29fc
maretol <maretol@outlook.com>
2024-11-02 19:25:27 +0900

    引用表現で引用元指定がない場合の挙動を修正

--- pages/components/middle/article_dom/blockquote.tsx
+++ pages/components/middle/article_dom/blockquote.tsx
@@ -9,25 +9,27 @@ export default async function Blockquote({
   attrs?: { [name: string]: string }
 }) {
   // text の cite:: 以降を引用元として解釈し、cite属性にセットする
-  const lastLine = text.split('cite::').pop()
   let citeText = ''
   let citeURL = null
-  if (lastLine) {
-    // lastLine は [引用元のテキスト](URL) という形式、または [引用元のテキスト] という形式
-    const urlMatch = lastLine.match(/\(([^)]+)\)/)
-    if (urlMatch) {
-      // 引用元テキストが [] で囲まれている場合それを優先。囲われてない場合はURL要素を除去して表示
-      const titleMatch = lastLine.match(/\[(.+)\]/)
-      citeText = titleMatch ? titleMatch[1] : lastLine.replace(urlMatch[0], '')
-      citeURL = urlMatch[1]
-    } else {
-      citeText = lastLine
-      // citeText が URL だけの場合はリンクのためURL要素もセット
-      if (lastLine.match(/https?:\/\/.+/)) {
-        citeURL = lastLine
+  if (text.includes('cite::')) {
+    const lastLine = text.split('cite::').pop()
+    if (lastLine) {
+      // lastLine は [引用元のテキスト](URL) という形式、または [引用元のテキスト] という形式
+      const urlMatch = lastLine.match(/\(([^)]+)\)/)
+      if (urlMatch) {
+        // 引用元テキストが [] で囲まれている場合それを優先。囲われてない場合はURL要素を除去して表示
+        const titleMatch = lastLine.match(/\[(.+)\]/)
+        citeText = titleMatch ? titleMatch[1] : lastLine.replace(urlMatch[0], '')
+        citeURL = urlMatch[1]
+      } else {
+        citeText = lastLine
+        // citeText が URL だけの場合はリンクのためURL要素もセット
+        if (lastLine.match(/https?:\/\/.+/)) {
+          citeURL = lastLine
+        }
       }
+      innerHTML = innerHTML.replace(`cite::${lastLine}`, '')
     }
-    innerHTML = innerHTML.replace(`cite::${lastLine}`, '')
   }
 
   return (
