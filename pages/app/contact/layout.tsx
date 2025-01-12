f46fd1f4d346d5665591b7ab049d12cdf394eb27
maretol <maretol@outlook.com>
2025-01-12 10:56:24 +0900

    add layouts

---
+++ pages/app/contact/layout.tsx
@@ -0,0 +1,5 @@
+import BaseLayout from '@/components/large/base_layout'
+
+export default function ContactLayout({ children }: { children: React.ReactNode }) {
+  return <BaseLayout>{children}</BaseLayout>
+}
---
+++ pages/app/tag/layout.tsx
@@ -0,0 +1,5 @@
+import BaseLayout from '@/components/large/base_layout'
+
+export default function TagLayout({ children }: { children: React.ReactNode }) {
+  return <BaseLayout>{children}</BaseLayout>
+}
