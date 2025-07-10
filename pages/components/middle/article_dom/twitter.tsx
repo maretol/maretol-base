022f184b74001aa8d84aa9550cef9bb146ee35b0
maretol <maretol@outlook.com>
2025-07-10 23:56:32 +0900

    キャッシュ追加

--- pages/components/middle/article_dom/twitter.tsx
+++ pages/components/middle/article_dom/twitter.tsx
@@ -1,14 +1,43 @@
 import { defaultSandbox } from '@/lib/static'
+import { getCloudflareContext } from '@opennextjs/cloudflare'
+import { cache } from 'react'
 
 export default async function TwitterArea({ twitterURL }: { twitterURL: string }) {
-  const twitterPublishURL = `https://publish.twitter.com/oembed?url=${twitterURL}`
-  const twitterPublish = await fetch(twitterPublishURL)
-  const twitterPublishJSON = await twitterPublish.json<{ html: string; width: number }>()
-
-  const twitterHTML = twitterPublishJSON.html
-  const width = twitterPublishJSON.width
-
   const sandbox = defaultSandbox + ' allow-presentation'
+  try {
+    const twitterPublishJSON = await fetchTweet(twitterURL)
 
-  return <iframe srcDoc={twitterHTML} sandbox={sandbox} allowFullScreen width={width} height={400}></iframe>
+    if (!twitterPublishJSON || !twitterPublishJSON.html || !twitterPublishJSON.width) {
+      return <p>Error: fetching tweet is error.</p>
+    }
+
+    const twitterHTML = twitterPublishJSON.html
+    const width = twitterPublishJSON.width
+
+    return <iframe srcDoc={twitterHTML} sandbox={sandbox} allowFullScreen width={width} height={400}></iframe>
+  } catch (error) {
+    console.error('Error fetching tweet:', error)
+    return <p>Error: fetching tweet is error.</p>
+  }
+}
+
+const fetchTweet = cache(fetchTweetOrigin)
+
+async function fetchTweetOrigin(tweetURL: string) {
+  const { env } = getCloudflareContext()
+  const tweetKey = 'tweet_' + tweetURL
+  const cache = await env.OGP_FETCHER_CACHE.get(tweetKey)
+  if (cache) {
+    const tweetData = (await JSON.parse(cache)) as { html: string; width: number }
+    return tweetData
+  }
+
+  const tweetPublishURL = `https://publish.twitter.com/oembed?url=${tweetURL}`
+  const tweetPublish = await fetch(tweetPublishURL)
+  if (!tweetPublish.ok) {
+    throw new Error(`Failed to fetch Twitter embed: ${tweetPublish.status} ${tweetPublish.statusText}`)
+  }
+  const tweetPublishJSON = await tweetPublish.json<{ html: string; width: number }>()
+  await env.OGP_FETCHER_CACHE.put(tweetKey, JSON.stringify(tweetPublishJSON), { expirationTtl: 60 * 60 * 24 }) // Cache for 24 hours
+  return tweetPublishJSON
 }
