865fe93d73f1289ea8a5adb9ca94d138fdedcc47
maretol <maretol@outlook.com>
2025-08-13 21:04:46 +0900

    loading追加

--- pages/app/page.tsx
+++ pages/app/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react'
 import BaseLayout from '@/components/large/base_layout'
+import { LoadingTopPage } from '@/components/large/loading_toppage'
 import TopPage from './toppage'
 
 export const dynamic = 'force-dynamic'
@@ -7,7 +8,7 @@ export const dynamic = 'force-dynamic'
 export default async function Mainpage() {
   return (
     <BaseLayout>
-      <Suspense fallback={<div>loading...</div>}>
+      <Suspense fallback={<LoadingTopPage />}>
         <TopPage />
       </Suspense>
     </BaseLayout>
