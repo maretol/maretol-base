626fc38db60e3cbdd4b7dbc39744f2acbda53844
maretol <maretol@outlook.com>
2025-10-04 09:49:26 +0900

    使わなくなったので削除

--- pages/lib/tag.ts
+++
@@ -1,7 +0,0 @@
-import { contentsAPIResult } from 'api-types'
-
-export function filterTags(firstTagContents: contentsAPIResult[], tagIDs: string[]) {
-  return firstTagContents.filter((content) => {
-    return content.categories.some((tag) => tagIDs.includes(tag.id))
-  })
-}
